9bf70c25ca348c52d9be8e5307ed16ec0626de7e
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-28 22:08:27 +0200

    update_address lisätty

---
+++ update_address.php
@@ -0,0 +1,34 @@
+<?php
+session_start(); // Aloita istunto
+$initials=parse_ini_file("./.ht.settings.ini");
+// Tarkista, onko käyttäjä kirjautunut sisään
+if(!isset($_SESSION['loggedin'])) {
+    echo "Kirjaudu sisään päivittääksesi osoitetietosi.";
+    exit;
+}
+// Muodosta yhteys tietokantaan
+$mysqli = new mysqli($initials["host"], $initials["user"], $initials["pass"], $initials["name"]);
+// Tarkistaa, onnistuiko yhteyden muodostaminen
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+// Suojaa käyttäjän syöttämät tiedot SQL-injektioita vastaan
+$username = $mysqli->real_escape_string($_POST['username']); 
+$email = $mysqli->real_escape_string($_POST['email']);
+
+// Päivittää tiedot tietokantaan
+$sql = "UPDATE accounts SET username='$username', email='$email' WHERE username='" . $_SESSION['name'] . "'";
+
+if ($mysqli->query($sql) === TRUE) {
+    header('Location: profile.php'); // Ohjaa käyttäjä takaisin profiilisivulle
+    echo "Tiedot päivitetty onnistuneesti!";
+    $_SESSION['name'] = $username;
+} else {
+    echo "Virhe: " . $sql . "<br>" . $mysqli->error;
+}
+}
+
+$mysqli->close();
+?>
