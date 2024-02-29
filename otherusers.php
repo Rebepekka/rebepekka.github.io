de6623ae1a357a0ce99a7149d85618d84e0b8d35
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 20:00:56 +0200

    fix

--- otherusers.php
+++ otherusers.php
@@ -8,6 +8,23 @@
 </head>
 <body>
 	<header></header>
+	
+	<nav class="navbar navbar-expand-xl navbar-dark bg-black">        
+            <div class="container-fluid">
+              <ul class="navbar-nav mr-auto">  
+                <li class="nav-item active">
+                  <a class="nav-link" href="home.php" style="font-size: 19px; filter: brightness(95%);">HOME</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="otherusers.php" style="font-size: 19px; filter: brightness(95%);">OTHER USERS</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="logout.php" style="font-size: 19px; filter: brightness(85%); color: gold;">LOG OUT</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
 	<div class="text">
 		<h1>OTHER USERS</h1>
 	</div>
--- profile.php
+++ profile.php
@@ -36,10 +36,10 @@ $stmt->close();
             <div class="container-fluid">
               <ul class="navbar-nav mr-auto">  
                 <li class="nav-item active">
-                  <a class="nav-link" href="home.php" style="font-size: 19px; filter: brightness(95%);">HOME</a>
-                </li>
-                <li class="nav-item active">
                   <a class="nav-link" href="otherusers.php" style="font-size: 19px; filter: brightness(95%);">OTHER USERS</a>
+                </li>
+				<li class="nav-item active">
+                  <a class="nav-link" href="home.php" style="font-size: 19px; filter: brightness(85%); color: gold;">BACK</a>
                 </li>
                 <li class="nav-item active">
                   <a class="nav-link" href="logout.php" style="font-size: 19px; filter: brightness(85%); color: gold;">LOG OUT</a>
