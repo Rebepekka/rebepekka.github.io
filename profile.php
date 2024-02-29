8acbce4bb7f3213fd30864954f202e3a3f963af7
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 16:39:35 +0200

    muutoksia css:ään

--- profile.php
+++ profile.php
@@ -32,9 +32,11 @@ $stmt->close();
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body>
-	<header></header>
+	<header>
+	<a href="index.html"><h1>BODYBUILDERS</h1></a>
+	</header>
 			<div class="headline">
-				<h1>Profile Page</h1>
+				<p>Profile Page</p>
 			</div>
                 <!-- Linkit uloskirjautumiseen ja etusivulle -->
 <div class="container">
