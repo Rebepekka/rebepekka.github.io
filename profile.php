368a672c507365c3048331b38e2b277c80a2c2a3
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 16:28:47 +0200

    boostrap lisätty

--- profile.php
+++ profile.php
@@ -26,6 +26,7 @@ $stmt->close();
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 		<link href="css/profilestyle.css" rel="stylesheet" type="text/css">
 		<title>Profile Page</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
@@ -86,5 +87,6 @@ $stmt->close();
 			</div>
 		</div>
 		</div>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 	</body>
 </html>
