1132344af79f1880163cff5f1defe5cd652f195d
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 15:30:02 +0200

    yhteyslinkki lisätty

--- register.php
+++ register.php
@@ -1,12 +1,13 @@
 <?php
 // Aloitetaan sessio
 session_start();
-$initials=parse_ini_file("./.ht.settings.ini");
+//$initials=parse_ini_file("./.ht.settings.ini");
 // Muodostetaan yhteys.
-$con=mysqli_connect($initials["host"], $initials["user"], $initials["pass"], $initials["name"]);
-if (mysqli_connect_errno()) { // Jos yhteydessä on virhe, pysäytä komentosarja ja näytä virhe.
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+include ("./connect.php"); // Linkki: luodaan yhteys.
+//$con=mysqli_connect($initials["host"], $initials["user"], $initials["pass"], $initials["name"]);
+//if (mysqli_connect_errno()) { // Jos yhteydessä on virhe, pysäytä komentosarja ja näytä virhe.
+//    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+//}
 // Tarkistetaan onko tiedot lähetetty, isset () -toiminto tarkistaa, onko tiedot olemassa.
 if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
     // Jos ei saa lähetettyjä tietoja...
