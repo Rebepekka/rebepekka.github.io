8e28fa88b9cc1877c9dbee2ce38238ce8e323441
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 20:13:07 +0200

    fix

--- otherusers.php
+++ otherusers.php
@@ -2,29 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/otherusersstyle.css" rel="stylesheet" type="text/css" />
 	<title>Bodybuilders | Other users</title>
 </head>
 <body>
 	<header></header>
-
-	<nav class="navbar navbar-expand-xl navbar-dark bg-black">        
-            <div class="container-fluid">
-              <ul class="navbar-nav mr-auto">  
-                <li class="nav-item active">
-                  <a class="nav-link" href="home.php" style="font-size: 19px; filter: brightness(95%);">HOME</a>
-                </li>
-                <li class="nav-item active">
-                  <a class="nav-link" href="otherusers.php" style="font-size: 19px; filter: brightness(95%);">OTHER USERS</a>
-                </li>
-                <li class="nav-item active">
-                  <a class="nav-link" href="logout.php" style="font-size: 19px; filter: brightness(85%); color: gold;">LOG OUT</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
 
 	<div class="text">
 		<h1>OTHER USERS</h1>
@@ -56,6 +39,5 @@ $con->close();
 ?>
 
 	</div>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
