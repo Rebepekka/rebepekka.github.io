b5fea24db6a61954af16f6feac188843f58d0acd
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 17:50:03 +0200

    muokkauksia ulkonäköön

--- otherusers.php
+++ otherusers.php
@@ -2,15 +2,35 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/otherusersstyle.css" rel="stylesheet" type="text/css" />
 	<title>Document</title>
 </head>
 <body>
-	<header></header>
-	<div class="text">
-		<h1>Other users</h1>
-	</div>
+<header>
+	<a href="index.html"><h1>BODYBUILDERS</h1></a>
+	</header>
+<div class="container">
+			<div class="headline">
+				<p>OTHER USERS</p>
+			</div>
+                <!-- Linkit uloskirjautumiseen ja etusivulle -->
+          <nav class="navbar navbar-expand-xl navbar-dark bg-black">        
+            <div class="container-fluid">
+              <ul class="navbar-nav mr-auto">  
+                <li class="nav-item active">
+                  <a class="nav-link" href="home.php" style="font-size: 19px; filter: brightness(95%);">HOME</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="otherusers.php" style="font-size: 19px; filter: brightness(95%);">OTHER USERS</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="logout.php" style="font-size: 19px; filter: brightness(85%); color: gold;">LOG OUT</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
 
 	<div class="users">
 
@@ -41,7 +61,11 @@ if ($stmt = $con->prepare('SELECT username FROM accounts')) {
 }
 $con->close();
 ?>
-
-	</div>
+</div>
+</div>
+<footer>
+    <address>BODYBUILDERS<br>Punttikuja 313<br>12345 PUNTTILA</address>
+</footer>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 </body>
 </html>
