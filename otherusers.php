6a38e8ea288e698660aef517272c4fdb4b26fe66
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 19:32:29 +0200

    fix

--- otherusers.php
+++ otherusers.php
@@ -16,13 +16,8 @@
 
 <?php
 session_start();
-// $initials=parse_ini_file("./.ht.settings.ini");
 // Muodostetaan yhteys.
 include ("./connect.php"); // Linkki: luodaan yhteys.
-// $con=mysqli_connect($initials["host"], $initials["user"], $initials["pass"], $initials["name"]);
-// if (mysqli_connect_errno()) {
-// 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-// }
 // Tarkistetaan, onko käyttäjä kirjautunut sisään
 if (!isset($_SESSION['loggedin'])) {
 	exit('Please log in first!');
