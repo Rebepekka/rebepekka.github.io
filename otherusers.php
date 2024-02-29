b134d8d7795189b110eeb79759964701b509b7cb
Rebepekka <rebekka23001@student.hamk.fi>
2024-02-29 19:54:10 +0200

    fix

--- otherusers.php
+++ otherusers.php
@@ -4,12 +4,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/otherusersstyle.css" rel="stylesheet" type="text/css" />
-	<title>Document</title>
+	<title>Bodybuilders | Other users</title>
 </head>
 <body>
 	<header></header>
 	<div class="text">
-		<h1>Other users</h1>
+		<h1>OTHER USERS</h1>
 	</div>
 
 	<div class="users">
